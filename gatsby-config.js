cf7607033a84e8d17dd3ad8402f49bee6e504e50
Jimmy Kane <jbk5603@psu.edu>
2023-07-19 18:37:06 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `FRC Team 5401 Wiki`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
